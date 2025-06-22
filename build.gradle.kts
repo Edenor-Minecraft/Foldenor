234965b08dae767ccf1832c47deb17e9c98d6c20
AltronMaxX <max06112004@gmail.com>
2025-06-22 14:26:00 +0400

    Update to 1.21.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import io.papermc.paperweight.tasks.RebuildGitPatches
 
 plugins {
     java // TODO java launcher tasks
-    id("io.papermc.paperweight.patcher") version "2.0.0-beta.16"
+    id("io.papermc.paperweight.patcher") version "2.0.0-beta.17"
 }
 
 paperweight {
